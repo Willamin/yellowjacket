9426725bcddf599c509ae1b26724000bb33e445c
Will Lewis <will@wflewis.com>
2020-04-22 13:11:11 -0400

    Improve interface

--- src/yellowjacket.cr
+++ src/yellowjacket.cr
@@ -24,8 +24,18 @@ class String
   end
 end
 
-ARGF
-  .gets_to_end
+case ARGV.size
+when 0
+  if STDIN.tty?
+    "Yellow Jacket accepts YAML from STDIN if no arguments are provided."
+    .puts(STDERR)
+  end
+  STDIN.gets_to_end
+when 1 then File.read(ARGV[0])
+else
+  "usage: #{PROGRAM_NAME} [FILENAME]".puts(STDERR)
+  exit(1)
+end
   .strip_yaml_comments
   .parse_as_yaml
   .to_json
